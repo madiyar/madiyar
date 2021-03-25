64424aac4dd626cea971d4da10a018b9a3186392
madiyar <MaBolatov@dar.kz>
2021-03-25 19:12:08 +0600

    init

---
+++ src/pages/about.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Link } from 'gatsby';
+
+const AboutPage = () => (
+  <div>
+    <h1>About</h1>
+    <Link to="/">Home</Link>
+  </div>
+);
+
+export default AboutPage;
