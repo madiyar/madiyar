debdf292bcc861ba61d4ec81524fcf55e4b8e09b
madiyar <MaBolatov@dar.kz>
2021-03-25 19:22:03 +0600

    styles init

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,15 @@
 import * as React from "react"
 import { Link } from "gatsby"
+import Layout from "../components/Layout"
 
-// styles
-const pageStyles = {
-  color: "#232129",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-
-// markup
 const IndexPage = () => {
   return (
-    <main style={pageStyles}>
-      <h1>Madiyar</h1>
-      <Link to="/about">About</Link>
-    </main>
+    <Layout>
+      <main>
+        <h1>Madiyar</h1>
+        <Link to="/about">About</Link>
+      </main>
+    </Layout>
   )
 }
 
